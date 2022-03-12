d205ff8c4ae28bb5eeac0325dd4782b75f2a27bf
skyoo1226 <you1226@naver.com>
2022-03-12 11:54:28 +0900

    데이터바인딩 초기 세팅 후 - 간단한 로그인 화면 만듬

--- app/src/main/java/com/skyoo/keepthetime_weeked_20220312/MainActivity.kt
+++ app/src/main/java/com/skyoo/keepthetime_weeked_20220312/MainActivity.kt
@@ -2,10 +2,18 @@ package com.skyoo.keepthetime_weeked_20220312
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.skyoo.keepthetime_weeked_20220312.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+
     }
 }
