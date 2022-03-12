8c8e199199ba99aa43ffe6dcc97452a9df93f291
skyoo1226 <you1226@naver.com>
2022-03-12 16:24:11 +0900

    회원가입화면: 입력값 추출 APIList : 회원 API 명세

--- app/src/main/java/com/skyoo/keepthetime_weeked_20220312/BaseActivity.kt
+++ app/src/main/java/com/skyoo/keepthetime_weeked_20220312/BaseActivity.kt
@@ -4,16 +4,25 @@ import android.content.Context
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.skyoo.keepthetime_weeked_20220312.api.APIList
+import com.skyoo.keepthetime_weeked_20220312.api.ServerAPI
+
+// 모든 화면이 공통으로 사용하는 것들
 
 abstract class BaseActivity : AppCompatActivity() {
 
 //    멤버변수 - this
     lateinit var mContext: Context
 
+    lateinit var apiList: APIList
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mContext = this
+
+        val retrofit = ServerAPI.getRetrofit()
+        apiList = retrofit.
     }
 //    셋업이벤트, 셋밸유
     abstract fun setupEvents()
