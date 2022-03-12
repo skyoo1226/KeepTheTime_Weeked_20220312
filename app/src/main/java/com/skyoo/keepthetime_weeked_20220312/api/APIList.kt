f4a74c48743c5af1f5c8f466520371b7c7bd204a
skyoo1226 <you1226@naver.com>
2022-03-12 16:03:37 +0900

    회원가입화면: 입력값 추출 APIList : 회원 API 명세

--- app/src/main/java/com/skyoo/keepthetime_weeked_20220312/api/APIList.kt
+++ app/src/main/java/com/skyoo/keepthetime_weeked_20220312/api/APIList.kt
@@ -1,11 +1,13 @@
 package com.skyoo.keepthetime_weeked_20220312.api
 
+import android.provider.ContactsContract
 import com.skyoo.keepthetime_weeked_20220312.datas.BasicResponse
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 // keepthetime.xyz  서버에 있는 기능에 접속하는 방법을 명시하는 인터페이스
 interface APIList {
@@ -16,6 +18,14 @@ interface APIList {
     fun postRequestLogin(
         @Field("Email") id: String,
         @Field("password") pw: String,
+    ) : Call<BasicResponse>
+
+    @FormUrlEncoded
+    @PUT("/user")
+    fun putRequestSignUp(
+        @Field("email") email: String,
+        @Field("password") pw: String,
+        @Field("nick_name") nick: String,
     ) : Call<BasicResponse>
 
 }
