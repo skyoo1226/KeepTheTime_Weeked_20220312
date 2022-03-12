a769399c0eb2f4895304c772cbd0cd9c1f134006
skyoo1226 <you1226@naver.com>
2022-03-12 14:43:18 +0900

    API를 요청 예시 : 임시 코드

--- app/src/main/java/com/skyoo/keepthetime_weeked_20220312/api/APIList.kt
+++ app/src/main/java/com/skyoo/keepthetime_weeked_20220312/api/APIList.kt
@@ -3,10 +3,14 @@ package com.skyoo.keepthetime_weeked_20220312.api
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
+// keepthetime.xyz  서버에 있는 기능에 접속하는 방법을 명시하는 인터페이스
 interface APIList {
 
+//    로그인 기능 : POST -  /user
+    @FormUrlEncoded
     @POST("/user")
     fun postRequestLogin(
         @Field("Email") id: String,
